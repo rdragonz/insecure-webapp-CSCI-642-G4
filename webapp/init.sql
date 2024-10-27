dfb80d55fce4b5e3272de2f8d625d95b3d423e49
Red Ziogas <ajz1369@rit.edu>
2024-10-27 03:14:32 -0400

    Add sample data into init file

--- webapp/init.sql
+++ webapp/init.sql
@@ -5,3 +5,18 @@ CREATE TABLE appointments (
     time VARCHAR(5) NOT NULL,
     description VARCHAR(255) NOT NULL
 );
+-- Sample data for October
+INSERT INTO appointments (date, time, description) VALUES 
+('2024-10-01', '09:00', 'Team Standup Meeting'),
+('2024-10-01', '14:00', 'Project Review with Client'),
+('2024-10-05', '11:00', 'Doctor Appointment'),
+('2024-10-10', '16:00', 'Code Review Session'),
+('2024-10-15', '10:30', 'Marketing Strategy Meeting');
+
+-- Sample data for November
+INSERT INTO appointments (date, time, description) VALUES 
+('2024-11-01', '13:00', 'Financial Report Review'),
+('2024-11-03', '09:30', 'New Hire Orientation'),
+('2024-11-10', '15:00', 'Product Demo'),
+('2024-11-12', '14:30', 'Quarterly Roadmap Update'),
+('2024-11-20', '08:00', 'Conference Call with Partner');
